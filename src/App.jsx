bb5e28d03cc570483722dbbe4f64db26ee41a65e
Pranavi <inkslinger5198@gmail.com>
2025-01-11 04:01:40 +0530

    finally progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Home from './components/homepage/Homepage';
 import Hero from './Pages/Services/s1/hero';
-import "../node_modules/slick-carousel/slick/slick-theme.css";
-import "../node_modules/slick-carousel/slick/slick.css";
-
 import './App.css';
 import Approach from './components/approach/Approach';
 import Services from './components/services/Services';
